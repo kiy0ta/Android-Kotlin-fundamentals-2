c9d82bab8520927e50e4fb2dbe6076f6ba79b040
kiyo <i.mitsuki19@gmail.com>
2023-02-26 03:28:01 +0900

    - target versionをAndroid13へ更新  - コメントを追加  - ボタンをクリックした時にトーストを表示させるよう実装

--- app/src/main/java/com/example/androidkotlinfundamentals2/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinfundamentals2/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.androidkotlinfundamentals2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
+// AppCompatActivityActivityは、古いバージョンの Android との下位互換性を提供しながら、
+// すべての最新の Android 機能をサポートする のサブクラスです。
+// できるだけ多くのデバイスとユーザーがアプリを利用できるようにするには、
+// 常にAppCompatActivityを使用してください。
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
     }
 }
